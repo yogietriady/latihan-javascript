50d5d0c41141c994b0e29d0cf8237bbcb5314a69
Yogie Triady <yogie.labcito@gmail.com>
2019-12-03 21:05:46 +0700

    latihan javascrip 1

---
+++ operator.js
@@ -0,0 +1,82 @@
+// Operator Aritmatika
+
+// No 1
+let satu = 10;
+let dua = 10;
+let hasil_penjumlahan = satu + dua;
+console.log(hasil_penjumlahan);
+
+// No 2
+satu = 20;
+dua = 10;
+let hasil_pengurangan = satu - dua;
+console.log(hasil_pengurangan);
+
+// No 3
+satu = 5;
+dua = 10;
+let hasil_perkalian = satu * dua;
+console.log(hasil_perkalian);
+
+// No 4
+satu = 30;
+kedua = 5;
+let hasil_pembagian = satu / dua;
+console.log(hasil_pembagian);
+
+// No 5
+satu = 55;
+dua = 2;
+let hasil_modulus = satu % dua;
+console.log(hasil_modulus);
+
+// Operator Perbandingan
+
+// No 1
+let foo;
+foo = "3" == 3;
+console.log(foo);
+foo = 4 == 3;
+console.log(foo);
+
+// No 2
+foo = "3" === 3;
+console.log(foo);
+foo = 3 === 3;
+console.log(foo);
+
+// No 3
+foo = 4 != 3;
+console.log(foo);
+foo = "3" != 3;
+console.log(foo);
+
+// No 4
+foo = 3 !== 3;
+console.log(foo);
+foo = "3" !== 3;
+console.log(foo);
+
+// No 5
+foo = 3 < 4;
+console.log(foo);
+foo = 4 < 3;
+console.log(foo);
+
+// No 6
+foo = 4 > 3;
+console.log(foo);
+foo = 3 > 4;
+console.log(foo);
+
+// No 7
+foo = 3 <= 3;
+console.log(foo);
+foo = 4 <= 3;
+console.log(foo);
+
+// No 8
+foo = 3 >= 3;
+console.log(foo);
+foo = 3 >= 4;
+console.log(foo);
